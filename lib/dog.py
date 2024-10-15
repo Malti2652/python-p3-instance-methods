5cd07b9ae171efe078dea92a1fa63fc55f9f2d95
Malti2652 <eddingtonibi@gmail.com>
2024-10-15 09:47:08 +0300

    instances

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
-class Dog:
-    # Class body goes here
+# class Dog:
+#     # Class body goes here
 
-    #Instance method definition
-    pass
+#     #Instance method definition
+#     pass
+
+class Dog:
+    def bark(self):
+        print("Woof!")
+        pass
+    def sit(self):
+        print("The dog is sitting.")
+
+fido = Dog()
+fido.bark()
